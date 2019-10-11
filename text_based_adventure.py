a3c91f8fea4a614de343407d7e8258d7653653ed
squeaky1273 <pjriddell22@gmail.com>
2019-10-11 14:07:56 -0700

    added start adventure

--- text_based_adventure.py
+++ text_based_adventure.py
@@ -1,17 +1,25 @@
-player_name = (input("What is your name?: "))
-player_occupation = (input("What is your occupation?: "))
-print("My name is {}. I am a {}.".format(player_name, player_occupation))
+class Story():
+    def __init__(self, player_name):
+        self.player_name = player_name
 
-class Story1():
-
-
-
-
-
-
+    def main(self):
+        player_name = (input("What is your name?: "))
+        print("My name is {}.".format(player_name))
 
 """
 decide = True
 while decide == True:
     choose = input("Enter:\nA for story 1, \nB for story 2, or \nc for story 3")
 """
+
+class Story1(Story):
+    def __init__(self, player_name):
+        super().__init__(player_name)
+
+    def start_adventure(self, player_name):
+        print("Welcome {}. You are a teenager who is working at a gas station.".format(player_name))
+
+
+
+if __name__ == "__main__":
+    pass
