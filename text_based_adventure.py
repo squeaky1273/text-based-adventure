61bbdd3ba4dba80256abcb114bea9c74d5b7a82c
squeaky1273 <pjriddell22@gmail.com>
2019-10-11 11:51:10 -0700

    commented out boolean

--- text_based_adventure.py
+++ text_based_adventure.py
@@ -1,3 +1,15 @@
-player_name = (input("What is your name?:"))
-player_occupation = (input("What is your occupation?:"))
+player_name = (input("What is your name?: "))
+player_occupation = (input("What is your occupation?: "))
 print("My name is {}. I am a {}.".format(player_name, player_occupation))
+
+
+
+
+
+
+
+"""
+decide = True
+while decide == True:
+    choose = input("Enter:\nA for story 1, \nB for story 2, or \nc for story 3")
+"""
