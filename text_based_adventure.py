069e99b50a2c13e818163833359b2e0ebb245f71
squeaky1273 <pjriddell22@gmail.com>
2019-10-11 11:58:06 -0700

    added story 1 class

--- text_based_adventure.py
+++ text_based_adventure.py
@@ -2,6 +2,8 @@ player_name = (input("What is your name?: "))
 player_occupation = (input("What is your occupation?: "))
 print("My name is {}. I am a {}.".format(player_name, player_occupation))
 
+class Story1():
+
 
 
 
