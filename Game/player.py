58f4692cddede8cc1040ef8fa57faaf8a940edf0
squeaky1273 <pjriddell22@gmail.com>
2019-10-25 15:12:59 -0700

    added init file

--- Game/player.py
+++ Game/player.py
@@ -1,13 +1,7 @@
-class Story():
+class Player():
     def __init__(self, player_name):
         self.player_name = player_name
 
-    def main(self):
+    def player(self):
         player_name = (input("What is your name?: "))
         print("My name is {}.".format(player_name))
-
-"""
-decide = True
-while decide == True:
-    choose = input("Enter:\nA for story 1, \nB for story 2, or \nc for story 3")
-"""
