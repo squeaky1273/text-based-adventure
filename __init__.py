bcb1e8f403515fea8815b515fb35d9cb157956c7
squeaky1273 <pjriddell22@gmail.com>
2019-10-25 15:30:47 -0700

    added world file

--- __init__.py
+++ __init__.py
@@ -9,7 +9,10 @@ class Weapon(Item):
 
 class Rock(Weapon):
     def __init__(self):
-        super.__init__()
+        super.__init__(name="Rock",
+                       description="It's the size of a fist. This should work for now.",
+                       value=0,
+                       damage=5)
 
 class Bottle(Weapon):
     def __init__(self):
